bf6c8f81d42271ba45a40e4b6c4795dd959cc05f
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-03-04 20:11:21 +0300

    Add project files.

---
+++ src/tobeto2A.RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,59 @@
+﻿using Application.Features.Brands.Constants;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using NArchitecture.Core.Application.Pipelines.Authorization;
+using NArchitecture.Core.Application.Pipelines.Logging;
+using NArchitecture.Core.Application.Pipelines.Performance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>, ISecuredRequest, ILoggableRequest, IIntervalRequest
+{
+    public string Name { get; set; }
+    public string Logo { get; set; }
+
+    // || 
+    public string[] Roles => new string[] { BrandsOperationClaims.Write, BrandsOperationClaims.Create };
+
+    public int Interval => 3;
+
+
+
+    // Brand.Update, Brand.Add, Brand.Delete, Brand.General
+
+    // Inner class
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
+        }
+
+        public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            await Task.Delay(5000);
+            // Thread.Sleep(5000) => Sync
+            await _brandBusinessRules.CarShouldNotExistsWithSameName(request.Name);
+            Brand brand = _mapper.Map<Brand>(request);
+
+            Brand addedBrand = await _brandRepository.AddAsync(brand);
+
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(addedBrand);
+
+            return createdBrandResponse;
+        }
+    }
+}
